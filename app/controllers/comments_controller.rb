6fc8c6d8aef0c94119727996b65d2dc188acdffa
KouSei089 <qq00891829@icloud.com>
2021-04-19 12:05:36 +0900

    association

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  def index
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.post_id = @post.id
+    @comment.save
+    # @comment = @post.comments.create(comment_params) で1行での記載も可。
+    redirect_to post_path(@post)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/new'
+  get 'comments/edit'
   root to: 'posts#index'
   resources :posts
+
+  resources :posts do
+    resources :comments
+  end
 end
